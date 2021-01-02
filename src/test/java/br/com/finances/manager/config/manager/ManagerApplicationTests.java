a3f0fd5bb9a227ad3794b47594e8d8c6207351d9
jass2125 <jair_anderson_bs@hotmail.com>
2021-01-02 17:02:36 -0300

    Criando estrutura do projeto

---
+++ src/test/java/br/com/finances/manager/config/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.finances.manager.config.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
